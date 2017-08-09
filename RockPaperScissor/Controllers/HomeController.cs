e8e2b2e95707b75817d4456fdfe22d2340d7d528
Guest User <Guest@epicodus-25.local>
2017-08-09 11:56:13 -0700

    this commit will make sure that all tests are working. model is working as per need. Need to add css styling to the images

---
+++ RockPaperScissor/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using RockPaperScissor.Models;
+using System.Collections.Generic;
+using System;
+
+namespace RockPaperScissor.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpPost("/play")]
+    public ActionResult Play()
+    {
+      string userChoice = Request.Form["choice"];
+      RockPaperScissorClass rps = new RockPaperScissorClass(userChoice);
+      return View("Index",rps);
+    }
+  }
+}
--- RockPaperScissor/Models/RockPaperScissor.cs
+++ RockPaperScissor/Models/RockPaperScissor.cs
@@ -15,40 +15,39 @@ namespace RockPaperScissor.Models
        Random r = new Random();
        _computerChoice = _inputs[r.Next(0, _inputs.Length)];
     }
-    public void SetComputerChoice(string computerChoice)
-    {
-      _computerChoice = computerChoice;
-    }
+    // public void SetComputerChoice(string computerChoice)
+    // {
+    //   _computerChoice = computerChoice;
+    // }
     public string GetComputerChoice()
     {
       return _computerChoice;
     }
     public string GetUserChoice()
     {
-      return _computerChoice;
+      return _userChoice;
     }
     public string Play()
     {
-      if(_userChoice == null || _computerChoice == null)
-      return "Plaese enter valid choices";
-      else if (_computerChoice != null && _userChoice != null)
+      string result= " " ;
+      if (_computerChoice != null && _userChoice != null)
       {
         if(_userChoice ==  _computerChoice)
-        return "Draw";
+        result="Draw";
         else if(_computerChoice == "Rock" && _userChoice == "Scissor")
-        return "Computer Wins";
+        result="Computer Wins";
         else if(_computerChoice == "Paper" && _userChoice == "Rock")
-        return "Computer Wins";
+        result= "Computer Wins";
         else if(_computerChoice == "Scissor" && _userChoice == "Paper")
-        return "Computer Wins";
+        result="Computer Wins";
         else if(_computerChoice == "Scissor" && _userChoice == "Rock")
-        return "User Wins";
+        result="User Wins";
         else if(_computerChoice == "Rock" && _userChoice == "Paper")
-        return "User Wins";
+        result="User Wins";
         else if(_computerChoice == "Paper" && _userChoice == "Scissor")
-        return "User Wins";
+        result= "User Wins";
       }
-        return "";
+        return result;
     }
   }
 }
