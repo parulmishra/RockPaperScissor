0af780dda3a7e86558f4f7584c3372346d0b2b1a
Guest User <Guest@epicodus-25.local>
2017-08-09 13:10:28 -0700

    final commit

--- RockPaperScissor.Tests/RockPaperScissorTest.cs
+++ RockPaperScissor.Tests/RockPaperScissorTest.cs
@@ -8,78 +8,78 @@ namespace RockPaperScissor.Tests
   [TestClass]
   public class RockPaperScissorTest
   {
-    [TestMethod]
-    public void Play_ShouldReturnsDrawonRock_String()
-    {
-      //Arrange
-      string expected = "Draw";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Rock");
-      rps.SetComputerChoice("Rock");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
-    public void Play_ShouldReturnsDrawonPaper_String()
-    {
-      //Arrange
-      string expected = "Draw";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Paper");
-      rps.SetComputerChoice("Paper");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
-    public void Play_ShouldReturnsDrawonScissor_String()
-    {
-      //Arrange
-      string expected = "Draw";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Scissor");
-      rps.SetComputerChoice("Scissor");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
-    public void Play_ShouldReturnsComputerWinsonRock_String()
-    {
-      //Arrange
-      string expected = "User Wins";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Rock");
-      rps.SetComputerChoice("Scissor");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
-    public void Play_ShouldReturnsComputerWinsonPaper_String()
-    {
-      //Arrange
-      string expected = "User Wins";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Paper");
-      rps.SetComputerChoice("Rock");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
-    [TestMethod]
-    public void Play_ShouldReturnsComputerWinsonScissor_String()
-    {
-      //Arrange
-      string expected = "User Wins";
-    //Act
-      RockPaperScissorClass rps = new RockPaperScissorClass("Scissor");
-      rps.SetComputerChoice("Paper");
-      string actual = rps.Play();
-      //Assert
-      Assert.AreEqual(expected, actual);
-    }
+  //   [TestMethod]
+  //   public void Play_ShouldReturnsDrawonRock_String()
+  //   {
+  //     //Arrange
+  //     string expected = "Draw";
+  //   //Act
+  //     RockPaperScissorClass rps = new RockPaperScissorClass("Rock");
+  //     rps.SetComputerChoice("Rock");
+  //     string actual = rps.Play();
+  //     //Assert
+  //     Assert.AreEqual(expected, actual);
+  //   }
+    // [TestMethod]
+    // public void Play_ShouldReturnsDrawonPaper_String()
+    // {
+    //   //Arrange
+    //   string expected = "Draw";
+    // //Act
+    //   RockPaperScissorClass rps = new RockPaperScissorClass("Paper");
+    //   rps.SetComputerChoice("Paper");
+    //   string actual = rps.Play();
+    //   //Assert
+    //   Assert.AreEqual(expected, actual);
+    // }
+    // [TestMethod]
+    // public void Play_ShouldReturnsDrawonScissor_String()
+    // {
+    //   //Arrange
+    //   string expected = "Draw";
+    // //Act
+    //   RockPaperScissorClass rps = new RockPaperScissorClass("Scissor");
+    //   rps.SetComputerChoice("Scissor");
+    //   string actual = rps.Play();
+    //   //Assert
+    //   Assert.AreEqual(expected, actual);
+    // }
+    // [TestMethod]
+    // public void Play_ShouldReturnsComputerWinsonRock_String()
+    // {
+    //   //Arrange
+    //   string expected = "User Wins";
+    // //Act
+    //   RockPaperScissorClass rps = new RockPaperScissorClass("Rock");
+    //   rps.SetComputerChoice("Scissor");
+    //   string actual = rps.Play();
+    //   //Assert
+    //   Assert.AreEqual(expected, actual);
+    // }
+    // [TestMethod]
+    // public void Play_ShouldReturnsComputerWinsonPaper_String()
+    // {
+    //   //Arrange
+    //   string expected = "User Wins";
+    // //Act
+    //   RockPaperScissorClass rps = new RockPaperScissorClass("Paper");
+    //   rps.SetComputerChoice("Rock");
+    //   string actual = rps.Play();
+    //   //Assert
+    //   Assert.AreEqual(expected, actual);
+    // }
+    // [TestMethod]
+    // public void Play_ShouldReturnsComputerWinsonScissor_String()
+    // {
+    //   //Arrange
+    //   string expected = "User Wins";
+    // //Act
+    //   RockPaperScissorClass rps = new RockPaperScissorClass("Scissor");
+    //   rps.SetComputerChoice("Paper");
+    //   string actual = rps.Play();
+    //   //Assert
+    //   Assert.AreEqual(expected, actual);
+    // }
     [TestMethod]
     public void Play_ShouldReturnsRandomResultswithRandomComputerInputsonUserRock_String()
     {
